169cc29b43cfa77ccff422490137af8d7645a888
Sathish-Veerasamy <sathish2181997@gmail.com>
2023-02-23 15:57:35 +0530

    button click done

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
+const[isClicked,setIsClicked]=useState(false);
 
   return (
     <div className="App" id="main">
       // Do not alter the main div
+
+      
+        
+        {isClicked &&  <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> }
+       
+        <button id="click" onClick={()=>{setIsClicked(true)}} >Button</button>
+
     </div>
   );
 }
